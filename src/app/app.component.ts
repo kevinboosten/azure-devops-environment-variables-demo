986391856be05e5f17417c33d1ac604e1d7c4f4c
Kevin Boosten <kboosten@ilionx.com>
2020-05-18 00:11:05 +0200

    ci: use environment variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'azure-devops-environment-variables-demo';
+  title = `is production: ${environment.production}, auth: ${environment.auth}, api: ${environment.apiEndpoint}`;
 }
